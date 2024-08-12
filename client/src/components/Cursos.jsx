e4d6f62de7dbc21c4e79074e12678a86995387e2
Victor Pompeu <102449860+VPompeu@users.noreply.github.com>
2024-08-12 14:37:09 -0300

    Update Cursos.jsx
    Mudando o titulo do dia 1

--- client/src/components/Cursos.jsx
+++ client/src/components/Cursos.jsx
@@ -7,7 +7,7 @@ function Cursos() {
     const cursos = [
         {
             id: 0,
-            title: 'Introdução à Docker',
+            title: 'Introdução ao Docker',
             author: 'Ministrante: Inaê Rosa',
             description: 'Explore o universo dos contêineres com uma abordagem da teoria à prática, utilizando imagens como Node e PostgreSQL. Diga adeus às horas gastas instalando softwares pesados na sua máquina local. Aprenda a orquestrar contêineres em Docker e evite o clássico "mas na minha máquina funciona". Ideal para maximizar sua produtividade e eficiência!'
         },
